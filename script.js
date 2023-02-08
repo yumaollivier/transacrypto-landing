606e7a838db1e6d81a717b568a11a61c7ef59a3b
Yuma <yuma.ollivier@hotmail.com>
2023-02-08 14:06:25 +0100

    BUGFIX - fixed form bugs

--- script.js
+++ script.js
@@ -1,73 +1,105 @@
 const forms = document.querySelectorAll('form');
+const notif = document.querySelector('.notification');
+
+const notificationHandler = (isError = false, message) => {
+  notif.classList.add(`visible${isError ? ' error' : ''}`);
+  notif.querySelector('p').innerHTML = message;
+  setTimeout(() => {
+    notif.classList.remove('visible');
+  }, 5000);
+};
+
+const formSubmit = e => {
+  e.preventDefault();
+
+  const formData = new FormData();
+  const firstName = document.querySelectorAll('input[name="firstName"]')[0]
+    .value;
+  const lastName = document.querySelectorAll('input[name="lastName"]')[0].value;
+  const email = document.querySelectorAll('input[name="email"]')[0].value;
+  if (firstName.length > 0 && lastName.length > 0 && email.length > 0) {
+    const formData = new FormData();
+    formData.append('FirstName', firstName);
+    formData.append('LastName', lastName);
+    formData.append('Email', email);
+
+    fetch('https://getform.io/f/e6abeb82-7c96-4a41-ba73-bde153dff6a3', {
+      method: 'POST',
+      body: formData,
+    })
+      .then(response => {
+        document
+          .querySelectorAll('input[type="text"], input[type="email"], textarea')
+          .forEach(input => {
+            input.value = null;
+          });
+        notificationHandler(
+          false,
+          '👌 Vos données ont bien été envoyées ! Merci beaucoup !'
+        );
+      })
+      .catch(error => {
+        notificationHandler(
+          true,
+          "😭 Vos coordonnées n'ont pas pu être envoyées..."
+        );
+      });
+  } else {
+    notificationHandler(
+      true,
+      '🤨 Merci de bien vouloir remplir tous les champs.'
+    );
+  }
+};
+
+const contactFormSubmit = e => {
+  e.preventDefault();
+
+  const firstName = document.querySelectorAll('input[name="firstName"]')[1]
+    .value;
+  const lastName = document.querySelectorAll('input[name="lastName"]')[1].value;
+  const email = document.querySelectorAll('input[name="email"]')[1].value;
+  const message = document.querySelector('textarea[name="message"]').value;
+  if (
+    firstName.length > 0 &&
+    lastName.length > 0 &&
+    email.length > 0 &&
+    message.length > 0
+  ) {
+    const formData = new FormData();
+    formData.append('FirstName', firstName);
+    formData.append('LastName', lastName);
+    formData.append('Email', email);
+    formData.append('Message', message);
+
+    fetch('https://getform.io/f/e6abeb82-7c96-4a41-ba73-bde153dff6a3', {
+      method: 'POST',
+      body: formData,
+    })
+      .then(response => {
+        document
+          .querySelectorAll('input[type="text"], input[type="email"], textarea')
+          .forEach(input => {
+            input.value = null;
+          });
+        notificationHandler(
+          false,
+          '👌 Vos données ont bien été envoyées ! Merci beaucoup !'
+        );
+      })
+      .catch(error => {
+        notificationHandler(
+          true,
+          "😭 Vos coordonnées n'ont pas pu être envoyées..."
+        );
+      });
+  } else {
+    notificationHandler(
+      true,
+      '🤨 Merci de bien vouloir remplir tous les champs.'
+    );
+  }
+};
 
 forms[0].addEventListener('submit', formSubmit);
 forms[1].addEventListener('submit', contactFormSubmit);
-
-function formSubmit(e) {
-  e.preventDefault();
-
-  const formData = new FormData();
-  formData.append('FirstName', document.querySelectorAll('input[name="firstName"]')[0].value);
-  formData.append('LastName', document.querySelectorAll('input[name="lastName"]')[0].value);
-  formData.append('Email', document.querySelectorAll('input[name="email"]')[0].value);
-
-  fetch('https://getform.io/f/e6abeb82-7c96-4a41-ba73-bde153dff6a3', {
-    method: 'POST',
-    body: formData,
-  })
-    .then(response => {
-      document.querySelectorAll('input').forEach(input => {
-        input.value = null;
-      });
-      document.querySelector('.banner-title').innerHTML = 'Merci Beaucoup !'
-      //   document.querySelector('.container__content').classList.add('hidden');
-      //   document.querySelector('.valid').classList.remove('hidden');
-      //   document.querySelector('.valid').classList.add('visible');
-    })
-    .catch(error => {
-        document.querySelectorAll('input').forEach(input => {
-            input.value = null;
-        });
-        document.querySelector('.banner-title').innerHTML = 'Vos coordonnées n\'ont pas été envoyées...'
-    //   document.querySelector('.container__content').classList.add('hidden');
-    //   document.querySelector('.invalid').classList.remove('hidden');
-    //   document.querySelector('.invalid').classList.add('visible');
-    });
-}
-
-function contactFormSubmit(e) {
-  e.preventDefault();
-
-  const formData = new FormData();
-  formData.append('FirstName', document.querySelectorAll('input[name="firstName"]')[1].value);
-  formData.append('LastName', document.querySelectorAll('input[name="lastName"]')[1].value);
-  formData.append('Email', document.querySelectorAll('input[name="email"]')[1].value);
-  formData.append(
-    'Message',
-    document.querySelector('textarea[name="message"]').value
-  );
-
-  fetch('https://getform.io/f/e6abeb82-7c96-4a41-ba73-bde153dff6a3', {
-    method: 'POST',
-    body: formData,
-  })
-    .then(response => {
-      document.querySelectorAll('input').forEach(input => {
-        input.value = null;
-      });
-      document.querySelector('textarea').value = null;
-      document.querySelector('.message').innerHTML = "Merci Beaucoup !"
-    //   document.querySelector('.container__content').classList.add('hidden');
-    //   document.querySelector('.valid').classList.remove('hidden');
-    //   document.querySelector('.valid').classList.add('visible');
-    })
-    .catch(error => {
-      document.querySelectorAll('input').forEach(input => {
-        input.value = null;
-      });
-        document.querySelector('.message').innerHTML = 'Vos coordonnées n\'ont pas été envoyées...'
-        //   document.querySelector('.container__content').classList.add('hidden');
-    //   document.querySelector('.invalid').classList.remove('hidden');
-    //   document.querySelector('.invalid').classList.add('visible');
-    });
-}
